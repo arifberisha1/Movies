b1c03989fb762b68a540fd1eb694c0642ea7b56a
AulonaLimani <aulona.limani@student.uni-pr.edu>
2023-05-06 00:34:54 +0200

    Working with Dates

--- client/src/actors/ActorForm.tsx
+++ client/src/actors/ActorForm.tsx
@@ -3,13 +3,15 @@ import TextField from "../forms/TextField";
 import Button from "../utils/Button";
 import {actorCreationDTO} from "./actors.model";
 import * as Yup from 'yup'
+import DateField from "../forms/DateField";
 
 export default function ActorForm(props: actorFormProps) {
     return (
         <Formik initialValues={props.model}
                 onSubmit={props.onSubmit}
                 validationSchema={Yup.object({
-                    name: Yup.string().required('This field is required').firstLetterUppercase()
+                    name: Yup.string().required('This field is required').firstLetterUppercase(),
+                    dateOfBirth: Yup.date().nullable().required('This field is required')
                 })}
         >
 
@@ -18,6 +20,9 @@ export default function ActorForm(props: actorFormProps) {
                     <TextField
                         field={"name"}
                         displayName={"Name"}/>
+                    <DateField
+                        field={"dateOfBirth"}
+                        displayName={"Date of Birth"}/>
 
                     <Button disabled={formikProps.isSubmitting} type={"submit"} >Save Changes</Button>
                     <a href={"/actors"} className={"btn btn-secondary"} >Cancel</a>
