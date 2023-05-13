6ea815f29e43fa659171c4e844d64aa48c10c03e
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2023-05-13 14:13:58 +0200

    Creating and getting genres from DB

--- client/src/genres/CreateGenres.tsx
+++ client/src/genres/CreateGenres.tsx
@@ -1,13 +1,29 @@
 import GenreForm from "./GenreForm";
+import {genreCreationDTO} from "./genres.model";
+import axios from "axios";
+import {urlGenres} from "../endpoints";
+import {useHistory} from "react-router-dom";
 
 export default function CreateGenres() {
+
+    const history = useHistory();
+    async function create(genre: genreCreationDTO){
+        try {
+            await axios.post(urlGenres, genre)
+            history.push('/genres');
+            window.location.reload();
+        }
+        catch (error){
+            console.error(error);
+        }
+    }
+
     return (
         <>
             <h3>Create Genre</h3>
             <GenreForm model={{name: ''}}
                        onSubmit={async value => {
-                           await new Promise(r => setTimeout(r, 3));
-                           console.log(value);
+                           await create(value);
                        }}
             />
         </>
--- client/src/genres/GenreForm.tsx
+++ client/src/genres/GenreForm.tsx
@@ -1,18 +1,17 @@
 import * as Yup from "yup";
 import {Form, Formik, FormikHelpers} from "formik";
 import TextField from "../forms/TextField";
-import {useHistory} from "react-router-dom";
 import Button from "../utils/Button";
 import {genreCreationDTO} from "./genres.model";
 
 export default function GenreForm(props: genreFormProps) {
-    const history = useHistory();
 
     return (
         <Formik initialValues={props.model}
                 onSubmit={props.onSubmit}
                 validationSchema={Yup.object({
-                    name: Yup.string().required('This field is required').firstLetterUppercase()
+                    name: Yup.string().required('This field is required')
+                    .max(50, "Max length is 50 characters").firstLetterUppercase()
                 })}>
             {(formikProps) => (
                 <Form>
