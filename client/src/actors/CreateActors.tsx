4820ce6f8bf5e770f3765af4cb27bc8f38a5bce5
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2023-05-16 01:14:15 +0200

    Actors entity - Fron End

--- client/src/actors/CreateActors.tsx
+++ client/src/actors/CreateActors.tsx
@@ -1,12 +1,47 @@
 import ActorForm from "./ActorForm";
+import {actorCreationDTO} from "./actors.model";
+import {useState} from "react";
+import DisplayErrors from "../utils/DisplayErrors";
+import {convertActorToFormData} from "../utils/formDataUtils";
+import axios from "axios";
+import {urlActors} from "../endpoints";
+import {useHistory} from "react-router-dom";
 
 export default function CreateActors() {
+
+    const [errors, setErrors] = useState<string[]>([]);
+    const history = useHistory();
+    async function create(actor: actorCreationDTO){
+        try {
+            const formData = convertActorToFormData(actor);
+
+            await axios({
+                method: 'post',
+                url: urlActors,
+                data: formData,
+                headers: {'Content-Type': 'multipart/form-data'}
+            });
+            history.push('/actors');
+            window.location.reload();
+        }
+        catch (error){
+            // @ts-ignore
+            if (error && error.response){
+                // @ts-ignore
+                setErrors(error.response.data);
+            }
+        }
+    }
+
     return (
         <>
             <h3>Create Actor</h3>
+
+            <DisplayErrors errors={errors} />
+
             <ActorForm
                 model={{name: '', dateOfBirth: undefined}}
-                onSubmit={values => console.log(values)}/>
+                onSubmit={async values => await create(values)}/>
         </>
     );
 }
