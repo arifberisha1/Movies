af674a1bbd66e9e69f37cbe4bb93c5747c60cf63
Arbresha Ejupi <arbresha.ejupi@student.uni-pr.edu>
2023-05-16 23:25:40 +0200

    Loading the Data on the Movie Creation Page - Frontend

--- client/src/endpoints.ts
+++ client/src/endpoints.ts
@@ -3,3 +3,5 @@ const baseURL = "http://localhost:5156/api";
 export const urlGenres = `${baseURL}/genres`;
 export const urlActors = `${baseURL}/actors`;
 export const urlMovieTheaters = `${baseURL}/movietheaters`;
+
+export const urlMovies = `${baseURL}/movies`;
--- client/src/movies/CreateMovie.tsx
+++ client/src/movies/CreateMovie.tsx
@@ -1,16 +1,33 @@
 import MovieForm from "./MovieForm";
 import {genreDTO} from "../genres/genres.model";
-import {movieTheaterDTO} from "../movietheaters/movieTheater.model";
+import {movieTheaterCreationDTO, movieTheaterDTO} from "../movietheaters/movieTheater.model";
+import {useEffect, useState} from "react";
+import axios, {AxiosResponse} from "axios";
+import {urlMovies} from "../endpoints";
+import {moviesPostGetDTO} from "./movies.model";
+import Loading from "../utils/Loading";
 
 export default function CreateMovie() {
 
-    const nonSelectedGenres: genreDTO[] = [{id: 1, name: 'Comedy'}, {id: 2, name: 'Drama'}]
-    const nonSelectedMovieTheaters: movieTheaterDTO[] =
-        [{id: 1, name: 'Albi Mall'}, {id: 2, name: 'Prishtina Mall'}]
+    const [nonSelectedGenres, setNonSelectedGenres] = useState<genreDTO[]>([]);
+    const [nonSelectedMovieTheaters, setNonSelectedMovieTheaters] = useState<movieTheaterDTO[]>([]);
+    const [loading, setLoading] =  useState(true);
+
+    useEffect(() =>{
+        axios.get(`${urlMovies}/postget`)
+            .then((response: AxiosResponse<moviesPostGetDTO>) => {
+            setNonSelectedGenres(response.data.genres);
+            setNonSelectedMovieTheaters(response.data.movieTheaters);
+            setLoading(false);
+        })
+    }, [])
+
+
 
     return (
         <>
             <h3>Create Movie</h3>
+            {loading ? <Loading/> :
             <MovieForm model={{title: '', inTheaters: false, trailer: ''}}
                        onSubmit={values => console.log(values)}
                        nonSelectedGenres={nonSelectedGenres}
@@ -18,7 +35,7 @@ export default function CreateMovie() {
                        nonSelectedMovieTheaters={nonSelectedMovieTheaters}
                        selectedMovieTheaters={[]}
                        selectedActors={[]}
-            />
+            />}
         </>
     );
 }
