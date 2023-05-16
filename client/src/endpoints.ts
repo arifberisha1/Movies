416b1c9232aa0e2fd4795b3db9ceb60aa26ca216
Blendi Povataj <blendi.povataj@gmail.com>
2023-05-16 18:58:28 +0200

    Displaying, editing and deleting Movie theaters in Front-End

--- client/src/endpoints.ts
+++ client/src/endpoints.ts
@@ -2,3 +2,4 @@ const baseURL = "http://localhost:5156/api";
 
 export const urlGenres = `${baseURL}/genres`;
 export const urlActors = `${baseURL}/actors`;
+export const urlMovieTheaters = `${baseURL}/movietheaters`;
--- client/src/movietheaters/IndexMovieTheaters.tsx
+++ client/src/movietheaters/IndexMovieTheaters.tsx
@@ -1,8 +1,33 @@
+import IndexEntity from "../utils/IndexEntity";
+import {movieTheaterDTO} from "./movieTheater.model";
+import {urlMovieTheaters} from "../endpoints";
+
 export default function IndexMovieTheaters() {
     return (
-        <>
-            <h3>Movie Theaters</h3>
-            <a href="/movietheaters/create" className={"btn btn-primary"}>Create Movie Theater</a>
-        </>
-    );
+        <IndexEntity<movieTheaterDTO>
+        url={urlMovieTheaters} createURL="movietheaters/create" title="Movie Theaters"
+        entityName="Movie Theater"
+        >
+
+            {(entities, buttons) => <>
+            <thead>
+            <tr>
+                <th></th>
+                <th>Name</th>
+            </tr>
+            </thead>
+                <tbody>
+                {entities?.map(entity => <tr key={entity.id}>
+                    <td>
+                        {buttons(`movietheaters/edit/${entity.id}`, entity.id)}
+                    </td>
+                    <td>
+                        {entity.name}
+                    </td>
+                </tr>)}
+                </tbody>
+
+            </>}
+        </IndexEntity>
+    )
 }
