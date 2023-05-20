df43f2c459e5dbf0110628d9959c58e70943075e
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2023-05-20 23:44:51 +0200

    FilterMoviesDTO

---
+++ server/DTOs/FilterMoviesDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace server.DTOs;
+
+public class FilterMoviesDTO
+{
+    public int Page { get; set; }
+    public int RecordsPerPage { get; set; }
+    public PaginationDTO PaginationDto {
+        get
+        {
+            return new PaginationDTO() { Page = Page, RecordsPerPage = RecordsPerPage };
+        }
+    }
+
+    public string Title { get; set; }
+    public int GenreId { get; set; }
+    public bool InTheaters { get; set; }
+    public bool UpcomingReleases { get; set; }
+}
