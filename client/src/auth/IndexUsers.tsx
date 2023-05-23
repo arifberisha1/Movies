fd3a0966393288b4a5ef62e6bd5a2e1f16a21cb3
AulonaLimani <aulona.limani@student.uni-pr.edu>
2023-05-23 16:16:15 +0200

    Displaying roles - Front End

--- client/src/auth/IndexUsers.tsx
+++ client/src/auth/IndexUsers.tsx
@@ -3,20 +3,29 @@ import {userDTO} from "./auth.models";
 import {urlAccounts} from "../endpoints";
 import Button from "../utils/Button";
 import customConfirm from "../utils/customConfirm";
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 import Swal from "sweetalert2";
+import {useEffect, useState} from "react";
 
-export default function IndexUsers(){
+export default function IndexUsers() {
 
-    async function makeAdmin(id: string){
+    const [adminIds, setAdminIds] = useState<string[]>([]);
+    useEffect(() => {
+        axios.get(`${urlAccounts}/getAdmins`)
+            .then((Emails: AxiosResponse<string[]>) => {
+                setAdminIds(Emails.data);
+            });
+    }, []);
+
+    async function makeAdmin(id: string) {
         await doAdmin(`${urlAccounts}/makeAdmin`, id);
     }
 
-    async function removeAdmin(id: string){
+    async function removeAdmin(id: string) {
         await doAdmin(`${urlAccounts}/removeAdmin`, id);
     }
 
-    async function doAdmin(url: string, id: string){
+    async function doAdmin(url: string, id: string) {
         await axios.post(url, JSON.stringify(id), {
             headers: {'Content-Type': 'application/json'}
         });
@@ -25,36 +34,42 @@ export default function IndexUsers(){
             title: 'Success',
             text: 'Operation finished correctly',
             icon: 'success'
+        }).then(() => {
+            window.location.reload();
         });
     }
 
-    return(
+    return (
         <IndexEntity<userDTO>
             title={"Users"}
             url={`${urlAccounts}/listUsers`}>
             {users => <>
                 <thead>
-                    <tr>
-                        <th></th>
-                        <th>Email</th>
-                    </tr>
+                <tr>
+                    <th></th>
+                    <th>Email</th>
+                    <th>Role</th>
+                </tr>
                 </thead>
                 <tbody>
                 {users?.map(user => <tr key={user.id}>
                     <td>
                         <Button
                             onClick={() => customConfirm(() => makeAdmin(user.id),
-                            `Do you wish to make ${user.email} an admin?`, 'Do it')}
+                                `Do you wish to make ${user.email} an admin?`, 'Do it')}
                         >Make Admin</Button>
 
                         <Button
                             className={"btn btn-danger ms-2"}
                             onClick={() => customConfirm(() => removeAdmin(user.id),
-                            `Do you wish to remove ${user.email} as an admin?`, 'Do it')}
+                                `Do you wish to remove ${user.email} as an admin?`, 'Do it')}
                         >Remove Admin</Button>
                     </td>
                     <td>
                         {user.email}
+                    </td>
+                    <td>
+                        {adminIds.includes(user.id) ? <>Admin</> : <>User</>}
                     </td>
                 </tr>)}
                 </tbody>
