ff04ad76053ca31d2d2185977a1d338e7fcff1d8
AulonaLimani <aulona.limani@student.uni-pr.edu>
2023-05-23 16:54:25 +0200

    Update Register User Endpoint

--- server/ApplicationDbContext.cs
+++ server/ApplicationDbContext.cs
@@ -33,4 +33,5 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<MoviesActors> MoviesActors { get; set; }
     public DbSet<MoviesGenres> MoviesGenres { get; set; }
     public DbSet<MoviesTheatersMovies> MovieTheatersMovies { get; set; }
+    public DbSet<UserDetails> UserDetails { get; set; }
 }
---
+++ server/Entities/UserDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Entities;
+
+public class UserDetails
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime Birthday { get; set; }
+    public string Gender { get; set; }
+    public string Address { get; set; }
+    [Required]
+    public string Email { get; set; }
+}
