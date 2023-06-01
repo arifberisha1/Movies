bd445fd30cc4134b281e92049a0bd8bcfa1995b3
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2023-06-01 20:19:45 +0200

    Server down error handling

---
+++ server/Controllers/ServerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("api/server")]
+public class ServerController : ControllerBase
+{
+    /// <summary>
+    /// Checks if the server is running.
+    /// </summary>
+    /// <returns>Returns 200 OK if the server is running.</returns>
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [HttpGet("running")]
+    public async Task<ActionResult> running()
+    {
+        return Ok();
+    }
+}
